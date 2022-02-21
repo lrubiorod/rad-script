9afc6e5561d7c439aa739825a169a2f40f8036a7
Luis Rubio <l.rubiorod@gmail.com>
2022-02-21 12:37:35 +0100

    feat: include StringParseXMLMap and example

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,38 @@ pub mod operators;
 
 pub fn dr() -> Vec<u8> {
     cbor_to_vec(&Value::Array(vec![
-        Value::Integer(RadonOpCodes::StringParseJSONMap as i128),
+        Value::Integer(RadonOpCodes::StringParseXMLMap as i128),
         Value::Array(vec![
             Value::Integer(RadonOpCodes::MapGetMap as i128),
-            Value::Text(String::from("bpi")),
+            Value::Text(String::from("dwml")),
+        ]),
+        Value::Array(vec![
+            Value::Integer(RadonOpCodes::MapGetArray as i128),
+            Value::Text(String::from("data")),
+        ]),
+        Value::Array(vec![
+            Value::Integer(RadonOpCodes::ArrayGetMap as i128),
+            Value::Integer(0),
         ]),
         Value::Array(vec![
             Value::Integer(RadonOpCodes::MapGetMap as i128),
-            Value::Text(String::from("USD")),
+            Value::Text(String::from("parameters")),
         ]),
         Value::Array(vec![
-            Value::Integer(RadonOpCodes::MapGetFloat as i128),
-            Value::Text(String::from("rate_float")),
+            Value::Integer(RadonOpCodes::MapGetMap as i128),
+            Value::Text(String::from("weather")),
+        ]),
+        Value::Array(vec![
+            Value::Integer(RadonOpCodes::MapGetArray as i128),
+            Value::Text(String::from("weather-conditions")),
+        ]),
+        Value::Array(vec![
+            Value::Integer(RadonOpCodes::ArrayGetMap as i128),
+            Value::Integer(2),
+        ]),
+        Value::Array(vec![
+            Value::Integer(RadonOpCodes::MapGetString as i128),
+            Value::Text(String::from("@weather-summary")),
         ]),
     ]))
         .unwrap()
